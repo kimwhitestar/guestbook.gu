a5288490018e58dcc2645520392589bf54273dc4
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-04-28 11:54:17 +0900

    2022.04.28 방명록 목록 (jsp '향상된For문' 에러 : looping에러발생되므로 일반for문 권장함)

---
+++ guest/GuestListCommand.java
@@ -0,0 +1,21 @@
+package guest;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import guest.database.GuestDAO;
+import guest.database.GuestVO;
+
+public class GuestListCommand implements GuestInterface {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GuestDAO dao = new GuestDAO();
+		List<GuestVO> vos = dao.searchGuestList();
+		request.setAttribute("vos", vos);
+	}
+}
---
+++ guest/database/GuestVO.java
@@ -0,0 +1,56 @@
+package guest.database;
+
+public class GuestVO {
+	private int idx;
+	private String name;
+	private String email;
+	private String homepage;
+	private String vDate;
+	private String hostIp;
+	private String content;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHomepage() {
+		return homepage;
+	}
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+	public String getvDate() {
+		return vDate;
+	}
+	public void setvDate(String vDate) {
+		this.vDate = vDate;
+	}
+	public String getHostIp() {
+		return hostIp;
+	}
+	public void setHostIp(String hostIp) {
+		this.hostIp = hostIp;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	
+}
