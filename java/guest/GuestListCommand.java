9de0efc9b081d3710f04db1309ec749f66158251
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-04-29 17:08:41 +0900

    방명록목록 - 페이징 블록 수업

--- java/guest/GuestListCommand.java
+++ java/guest/GuestListCommand.java
@@ -25,6 +25,8 @@ public class GuestListCommand implements GuestInterface {
 		 *   3. 총 페이지 수 구하기 : totPage => totRecCnt % pageSize (몫의 정수값이 0이면 정수, 몫의 정수값이 아니면 정수값 + 1)
 		 * 	 4. 현재페이지의 시작 인덱스 : startIndexNo => (page -1) * pageSize
 		 * 	 5. 화면에 보이는 방문소감의 시작번호 : curScrStartNo = totRecCnt - startIndexNo
+		 *	 6. 블록페이징처리
+		 *
 		 */
 		
 		//0. 현 페이지
@@ -37,7 +39,7 @@ public class GuestListCommand implements GuestInterface {
 		int totRecCnt = dao.totRecCnt();
 		
 		//3. 총 페이지 수
-		int totPage = (totRecCnt % pageSize)==0 ? (totRecCnt / pageSize) : (totRecCnt + pageSize)+1;
+		int totPage = (totRecCnt % pageSize)==0 ? (totRecCnt / pageSize) : (totRecCnt / pageSize)+1;
 		
 		//4. 현재페이지 시작 인덱스
 		int startIndexNo = (pageNo -1) * pageSize;
@@ -45,12 +47,25 @@ public class GuestListCommand implements GuestInterface {
 		//5. 방문소감의 시작번호
 		int curScrStartNo = totRecCnt - startIndexNo;
 
+		//6. 블록페이징처리시 아래 내용 추가 (한블럭에 3page씩 보여준다) : blockSize
+		int blockSize = 3;
+		
+		//7. 현재 블럭 위치 (첫번째 블럭 = 0) : curBlock 
+		int curBlock = (pageNo - 1) / blockSize;
+		
+		//8. 마지막 블럭 : lastBlock
+		int lastBlock = (totPage % blockSize)==0 ? (totPage / blockSize) - 1 : (totPage / blockSize);
+		
+		
 		//한 페이징에 표시할 레코드 검색
 		List<GuestVO> vos = dao.searchGuestList(startIndexNo, pageSize);
 		request.setAttribute("vos", vos);
 		request.setAttribute("curScrStartNo", curScrStartNo);
 		request.setAttribute("pageNo", pageNo);
-		request.setAttribute("pageSize", pageSize);
 		request.setAttribute("totPage", totPage);
+		//request.setAttribute("pageSize", pageSize);
+		request.setAttribute("blockSize", blockSize);
+		request.setAttribute("curBlock", curBlock);
+		request.setAttribute("lastBlock", lastBlock);
 	}
 }
