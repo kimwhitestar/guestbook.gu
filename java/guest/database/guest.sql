a9aa9b5fd4ce2501948c6a73679fa3f05073dcd3
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-15 20:14:34 +0900

    Add files via upload

--- java/guest/database/guest.sql
+++ java/guest/database/guest.sql
@@ -1,7 +1,9 @@
 show tables;
+--drop table guest
 create table guest (
 	idx 	int not null auto_increment primary key, /*방명록 번호(pk)*/
-	name 	varchar(20) not null,	/*방문자 이름*/
+	mid		varchar(20),			/*회원 아이디*/
+	name 	varchar(20) not null,	/*방문자,회원 이름*/
 	email 	varchar(60),			/*방문자 이메일*/
 	homepage varchar(60),			/*방문자 홈페이지*/
 	vDate	datetime default now(), /*방문일(작성일)*/
@@ -9,6 +11,10 @@ create table guest (
 	content text not null			/*방명록 내용*/
 );
 desc guest;
-insert into guest values (default, '관리자', 'aaa@a.kr', 'cafe.daum.javaspringframework', default, '192.168.0.80', '방명록 서비스 시작');
-insert into guest values (default, '홍길동', 'hkd@x.f', 'hompage://none', default, '192.168.0.18', '내용없음');
-insert into guest values (default, '김말숙', 'kms@b.b.b.kr', 'bbq.chicken.kr', default, '192.168.0.120', '방명록 접속');
+
+--delete from guest;
+insert into guest values (default, 'pkl2','박김이2', 'pkl2@naver.com', 'http://naver.com/pkl2', default, 'localhost', '방명록 접속2');
+insert into guest values (default, 'pkl1','박김이1', 'pkl1@naver.com', 'http://naver.com/pkl1', default, 'localhost', '방명록 접속3');
+insert into guest values (default, 'pkl2','박김이2', 'pkl2@naver.com', 'http://naver.com/pkl2', default, 'localhost', '방명록 접속4');
+insert into guest values (default, null,'김말숙2', 'kms2@a.b.c.com', 'http://a.b.c.com/kms', default, '192.160.72.104', '방명록 test');
+insert into guest values (default, null,'홍길동9', 'hkd9@a.b.c.com', 'http://a.b.c.com/hkd9', default, '500.085.99.18', '테스트');
